0226dec96b525d162e7d9930c12718c1688658a4
Kim Dien <20520442@gm.uit.edu.vn>
2024-05-13 16:13:58 +0700

    Dev_"migrate dtos to new .net standard project"

--- Assignment_1/Dtos/RegisterRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assignment_1.Dtos
-{
-    public class RegisterRequestDto
-    {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
--- Assignment_1/Dtos/RegisterSuccessResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assignment_1.Dtos
-{
-    public class RegisterSuccessResponseDto
-    {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string AccessToken { get; set; }
-    }
-}
