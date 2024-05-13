b36ae4cbd488d42b362176b5abd4db96e121ca1e
Kim Dien <20520442@gm.uit.edu.vn>
2024-05-13 16:14:47 +0700

    Dev_"Migrate dtos to .net standard project 2"

---
+++ Dto/RegisterSuccessResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dtos
+{
+    public class RegisterSuccessResponseDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
