ff1b4a72e76508ca76559d44e303f3489ef32e5c
Kim Dien <20520442@gm.uit.edu.vn>
2024-05-15 10:29:46 +0700

    Dev_"Add Variant Dto, Remove Product field in model to avoid cycle reference"

--- Rookies_EcommerceWebsite/Data/Entities/Variant.cs
+++ Rookies_EcommerceWebsite/Data/Entities/Variant.cs
@@ -11,6 +11,5 @@ namespace Rookies_EcommerceWebsite.Data.Entities
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
         public uint Stock { get; set; }
-        public Product Product { get; set; }
     }
 }
