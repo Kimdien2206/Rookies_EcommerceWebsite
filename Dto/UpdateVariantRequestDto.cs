62a39d5040c553a32f4e36a880ad2e4cd0f91ffa
Kim Dien <20520442@gm.uit.edu.vn>
2024-05-15 11:58:09 +0700

    Dev_"Update service logic"

---
+++ Dto/UpdateVariantRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dtos
+{
+    public class UpdateVariantRequestDto
+    {
+        public string Name { get; set; }
+        public uint Stock { get; set; }
+    }
+
+}
