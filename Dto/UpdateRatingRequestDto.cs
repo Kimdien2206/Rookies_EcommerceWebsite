eb70aa48de4d951694f78f9a91769951cf45a7d2
Kim Dien <20520442@gm.uit.edu.vn>
2024-05-16 16:03:49 +0700

    Dev_"Add rating Dto"

---
+++ Dto/UpdateRatingRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dtos
+{
+    public class UpdateRatingRequestDto
+    {
+        public string Content { get; set; }
+    }
+}
