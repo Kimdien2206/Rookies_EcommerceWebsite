83470db710dcd3a92f913f927ffda4c1f41d42a3
Kim Dien <20520442@gm.uit.edu.vn>
2024-05-26 00:20:26 +0700

    Dev_"Refactor file"

--- Rookies_EcommerceWebsite/Data/Entities/User.cs
+++ Rookies_EcommerceWebsite/Data/Entities/User.cs
@@ -5,7 +5,6 @@ namespace Rookies_EcommerceWebsite.Data.Entities
 {
     public class User : IdentityUser
     {
-
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } 
         public string Address { get; set; }
