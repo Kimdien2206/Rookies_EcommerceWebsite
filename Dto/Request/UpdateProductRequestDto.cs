d3846b6161bc79bdc5fec853f2f93d9bbbda63c5
Kim Dien <20520442@gm.uit.edu.vn>
2024-05-28 17:45:08 +0700

    Dev_"Remove Id field in update dto"

--- Dto/Request/UpdateProductRequestDto.cs
+++ Dto/Request/UpdateProductRequestDto.cs
@@ -6,7 +6,6 @@ namespace Dtos
 {
     public class UpdateProductRequestDto
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string[] Images { get; set; }
