34d69db87fc1e937af4f45ba14d84b583445cbc4
Kim Dien <20520442@gm.uit.edu.vn>
2024-06-04 17:22:06 +0700

    Dev_"Add logout logic"

--- admin/src/pages/AdminPage.tsx
+++ admin/src/pages/AdminPage.tsx
@@ -1,23 +1,38 @@
-import { Layout } from "antd";
+import { Button, Layout, Space } from "antd";
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
 } from "@ant-design/icons/lib/icons";
 
 import Sider from "antd/es/layout/Sider";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import NavBar from "../component/NavBar";
 import { Content, Header } from "antd/es/layout/layout";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Scrollbars from "react-custom-scrollbars";
+import Cookies from "js-cookie";
+import LocalStorage from "../helper/localStorage";
+import { AppContext } from "../context/AppContext";
 
 const AdminPage = () => {
   const [collapsed, setCollapsed] = useState<boolean>(false);
+  const {setUser} = useContext(AppContext);
+  const nav = useNavigate();
 
+  const handleLogout = () => {
+    Cookies.remove("access_token");
+    LocalStorage.deleteItem("user");
+    setUser(undefined);
+    nav('/')
+  }
   return (
     <Layout style={{ width: "100%", height: "100vh" }}>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <NavBar />
+      <Sider trigger={null}  collapsible collapsed={collapsed}>
+        <Space direction="vertical" style={{justifyContent: "space-between"}}>
+          <NavBar />
+          <Button onClick={handleLogout}>Log out</Button>
+
+        </Space>
       </Sider>
       <Layout className="site-layout">
         <Header style={{ padding: 0, background: "white" }}>
